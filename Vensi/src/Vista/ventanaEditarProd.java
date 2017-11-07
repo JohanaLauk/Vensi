e387dd441cb2c400a48855a0417c0c059fc47709
biancamist <bianca.mistrorigo@gmail.com>
2017-11-07 15:17:22 -0300

    Excepciones en Modificar Clave Sólo tipeo de números - Ventana Nuevo y Editar Producto

--- Vensi/src/Vista/ventanaEditarProd.java
+++ Vensi/src/Vista/ventanaEditarProd.java
@@ -259,7 +259,8 @@ public class ventanaEditarProd extends javax.swing.JFrame
         if((c < '0' || c > '9') && 
                 (c != java.awt.event.KeyEvent.VK_BACK_SPACE) && 
                 (c != '.' || txfdEditarPrecioCosto.getText().contains("."))) 
-            evt.consume();        
+            evt.consume();       
+        
     }//GEN-LAST:event_txfdEditarPrecioCostoKeyTyped
 
     private void txfdEditarPrecioVentaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txfdEditarPrecioVentaKeyTyped
